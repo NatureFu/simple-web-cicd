343987df49efa77ee0de5d5ad25e2c918bd556d7
NatureFu <Fuziran415@outlook.com>
2026-06-21 20:38:32 +0800

    v2.0 update

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello CI/CD v1.0 - 姓名：付自然 学号：2440664307"
+    return "Hello CI/CD v2.0 - 姓名：付自然 学号：2440664307"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8888)
+    app.run(host="0.0.0.0", port=5000)
 
